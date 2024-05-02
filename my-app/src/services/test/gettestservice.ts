d01c591d322e6f96e839583771c6249116fa8fd1
Mithilesh-zenmonk1151 <mithilesh.1151@zenmonk.tech>
2024-05-02 18:50:14 +0530

    VBH

--- my-app/src/services/test/gettestservice.ts
+++ my-app/src/services/test/gettestservice.ts
@@ -3,7 +3,7 @@ import type { FieldValues } from "react-hook-form";
 const fetchTests = async (inputs: FieldValues) => {
     try {
         console.log("IIINNNPPUUUTT", inputs);
-        const response = await axios.get("api/test/getTest", { params: inputs });
+        const response = await axios.get("api/test/gettest", { params: inputs });
         console.log(response.data);
         return response.data;
     } catch (error) {
